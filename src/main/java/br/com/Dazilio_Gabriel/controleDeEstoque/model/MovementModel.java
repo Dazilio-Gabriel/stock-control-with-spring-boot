3af1f648023992294963c74e7f5ccb2ef387cfed
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-11-10 09:48:17 -0300

    classes produtos e movimentacao implementadas com construtor, getter e setter e com as etiquetas do mongoDB implementadas

--- src/main/java/br/com/Dazilio_Gabriel/controleDeEstoque/model/MovementModel.java
+++ src/main/java/br/com/Dazilio_Gabriel/controleDeEstoque/model/MovementModel.java
@@ -1,6 +1,65 @@
 package br.com.Dazilio_Gabriel.controleDeEstoque.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+
+@Document(collection = "movements")
 public class MovementModel {
 
+    @Id
+    private String id;
+    private LocalDate data;
+    private  String tipo;
+    private  Integer quantidade;
 
+    @DBRef private ProductModel product;
+
+    public MovementModel(){
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "MovementModel{" +
+                "id='" + id + '\'' +
+                ", data=" + data +
+                ", tipo='" + tipo + '\'' +
+                ", quantidade=" + quantidade +
+                ", product=" + product +
+                '}';
+    }
 }
+
--- src/main/java/br/com/Dazilio_Gabriel/controleDeEstoque/model/ProductModel.java
+++ src/main/java/br/com/Dazilio_Gabriel/controleDeEstoque/model/ProductModel.java
@@ -1,4 +1,50 @@
 package br.com.Dazilio_Gabriel.controleDeEstoque.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
 public class ProductModel {
+
+    @Id
+    private  String id;
+    private  String nome;
+    private String descricao;
+    private Integer estoqueAtual; // <-- ADICIONE ESTE
+
+    public ProductModel() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductModel{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
 }
