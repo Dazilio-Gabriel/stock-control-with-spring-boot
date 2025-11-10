7813a7671a0148d4cb6f81e00d29d58d99c74b6d
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-11-10 18:12:09 -0300

    metodos dos produtos feitos

--- src/main/java/br/com/Dazilio_Gabriel/controleDeEstoque/service/StockService.java
+++ src/main/java/br/com/Dazilio_Gabriel/controleDeEstoque/service/StockService.java
@@ -4,7 +4,12 @@ package br.com.Dazilio_Gabriel.controleDeEstoque.service;
 import br.com.Dazilio_Gabriel.controleDeEstoque.model.ProductModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
 
 @Service
 public class StockService {
@@ -18,15 +23,30 @@ public class StockService {
 
     public void inserirProdutos(ProductModel produto) {
         this.mongoTemplate.save(produto);
-
+        System.out.println("produto salvo: " + produto.getNome());
     }
 
-    public void listarTodosOsProdutos(ProductModel produto) {
-
+    public List<ProductModel> listarTodosOsProdutos() {
+        return this.mongoTemplate.findAll(ProductModel.class);
     }
 
-    public void removerProdutos(ProductModel produto) {
+    public void removerProdutos(String idProduto) {
+        Query query = new Query(Criteria.where("id").is(idProduto));
 
+        this.mongoTemplate.remove(query, ProductModel.class);
+        System.out.println("produto removido: " + idProduto);
+    }
+
+    public void atualizarProduto(String idProduto, ProductModel dadosNovos) {
+        Query query = new Query(Criteria.where("id").is(idProduto));
+
+        Update update = new Update();
+        update.set("nome", dadosNovos.getNome());
+        update.set("descricao", dadosNovos.getDescricao());
+        update.set("estoqueAtual", dadosNovos.getEstoqueAtual());
+
+        this.mongoTemplate.updateFirst(query, update, ProductModel.class);
+        System.out.println("produto atualizado: " + dadosNovos.getNome());
     }
 
 }
